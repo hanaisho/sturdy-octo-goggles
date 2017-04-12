6281a01828adbf5924cb214d0e9b94faf1b942aa
hanai <sho@hanaishounoMacBook-Pro.local>
2017-04-12 22:44:07 +0900

    てst

--- AlarmApp/hanaViewController.swift
+++ AlarmApp/hanaViewController.swift
@@ -12,6 +12,10 @@ class hanaViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
